4c3f08d81a35481077d00852043efa6774aa6240
Prashant Baid <prashantvbaid@gmail.com>
2016-02-17 15:28:23 +0530

    updated algorithm and added callback feature

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 var maj_index = 0;
 var count = 1;
-var candidate = function(arr) {
-  for(var i = 0; i < arr.length; i++) {
-    if(arr[maj_index] == arr[i])
-      count++;
-    else {
-      count --;
-    }
-    if(count == 0) {
-      maj_index = i;
-      count = 1;
-    }
-  }
-  return arr[maj_index];
+var elect = function(arr, cb) {
+    process.nextTick(function() {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[maj_index] == arr[i])
+                count++;
+            else {
+                count--;
+            }
+            if (count == 0) {
+                maj_index = i;
+                count = 1;
+            }
+        }
+        isMajority(arr, arr[maj_index], cb);
+    });
+
 }
 
-console.log(candidate([1,3,3,2,1,2,3,4,4]));
+function isMajority(arr, candidate, cb) {
+    var count = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == candidate)
+            count++;
+    }
+    if (count > arr.length / 2)
+        cb(null, true, candidate);
+    else {
+        cb(null, false, candidate);
+    }
+
+};
+module.exports = elect;
